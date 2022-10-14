cb0ad33c1bc55ad9818cedc55d6f6fe1a8caa271
FernandodotDev <fernandodossantos005@gmail.com>
2022-10-14 16:02:31 -0300

    tet

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -2,7 +2,7 @@
 
 describe("My First Test", () => {
   it("visits the app root url", () => {
-    cy.visit("/");
+    cy.visit("https://apoioaplv.netlify.app/");
     cy.contains("h1", "You did it!");
   });
 });
