73d645c5297fa24bf2c72fd053e92b790f3269a7
FernandodotDev <fernandodossantos005@gmail.com>
2022-10-14 16:09:12 -0300

    t

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,8 +1,9 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("visits the app root url", () => {
-    cy.visit("https://apoioaplv.netlify.app/");
-    cy.contains("h1", "You did it!");
-  });
+describe('html-pages', () => {
+  it('loads the index page', () => {
+    cy.visit('/')
+    cy.contains('Index page')
+  })
+
 });
